a0655e0373970c28ebf228d5a014edc4d8842864
Ashique-Desai <desai.ashique@gmail.com>
2022-03-29 16:43:09 +0530

    Hero text reduced

--- components/BannerMens.js
+++ components/BannerMens.js
@@ -19,25 +19,15 @@ export default function BannerMens() {
       {/* experimental */}
       <div className='w-1/4 absolute' >
         <div className='bottom-left-aligned lg:ml-16 ml-6 lg:mb-20 mb-12 z-[40] text-white'>
-          <CallToAction
+          <CallToAction 
             title='Autumn Winter 2022 Menswear Show'
             description='AMIRI’s singular journey cornerstones the 
-          Autumn-Winter 2022 collection, which marks Mike Amiri’s debut 
-          runway show in his home of Los Angeles – a 
-          proud moment for him and his team.'
+          Autumn-Winter 2022 collection'
           />
         </div>
 
       </div>
-      {/* <div className='w-1/4 absolute' >
-        <CallToAction 
-          title='Autumn Winter 2022 Menswear Show'
-          description='AMIRI’s singular journey cornerstones the 
-          Autumn-Winter 2022 collection, which marks Mike Amiri’s debut 
-          runway show in his home of Los Angeles – a 
-          proud moment for him and his team.'
-        />
-      </div> */}
+      
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -21,15 +21,14 @@ export default function Home({ products }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;400;700&display=swap" rel="stylesheet" />
       </Head>
-      {/* <BannerMensCopy /> */}
       <BannerMens />
       <SectionMens />
-      {/* <SectionMensCopy /> */}
       <ProductListMens products={products} />
       <BannerWomens />
       <ProductListWomens products={products} />
       <SectionAccessories />
-    </>)
+    </> 
+  )
 }
 
 export async function getStaticProps() {
