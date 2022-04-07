8d835d4362937d2db100355ccc53a88f416b032b
Ashique-Desai <desai.ashique@gmail.com>
2022-04-07 19:20:23 +0530

    Product grid cols 2 - mobile

--- components/ProductListMens.js
+++ components/ProductListMens.js
@@ -8,7 +8,7 @@ const ProductListMens = ({ products }) => {
   return (
     <>
       <TextHeadingBlock text='PRE-SPRING 2022 MENSWEAR' />
-      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-0">
+      <div className="grid grid-cols-2 gap-y-10 gap-x-0 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-0">
         {
           products.map(product => (
             <ProductCard key={product.node.id} product={product} />
