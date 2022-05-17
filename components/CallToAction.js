fc5782470a0eedad261101e0ab4fae4b68fa920a
Ashique-Desai <desai.ashique@gmail.com>
2022-05-17 19:49:45 +0530

    rolling back changes to CallToAction.js

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -5,19 +5,19 @@ import Link from 'next/link'
 export default function CallToAction(props) {
     
     return (
-        <div className='mix-blend-difference'>
-            {/* added back classes: text-white */}
-            <h4 className='text-lg mb-1 mt-2 sm:mb-7 uppercase font-bold text-white'>{props.title}</h4>
-            {/* added back classes: text-white */}
-            <p className="mb-7 text-xs text-white">{props.description}</p>
+        <>
+            {/* removed classes: text-white */}
+            <h4 className='text-lg mb-1 mt-2 sm:mb-7 uppercase font-bold'>{props.title}</h4>
+            {/* removed classes: text-white */}
+            <p className="mb-7 text-xs">{props.description}</p>
             <Link  href={{pathname: props.url}}>
-                 {/* removed classes: border-white color-amiri-white  */}
-                <button className='bg-transparent text-white hover:text-black hover:bg-white border-[1px] 
+                 {/* removed classes: border-white color-amiri-white text-white */}
+                <button className='bg-transparent hover:text-black hover:bg-white border-[1px] 
                 border-white sm:border-white rounded px-14 py-2 h-16 text-sm w-full sm:max-w-[200px] ml-auto mr-auto'>
                     DISCOVER
                 </button>
             </Link>
-        </div>
+        </>
     )
 }
 
