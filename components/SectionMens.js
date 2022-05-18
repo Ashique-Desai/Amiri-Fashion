5b7a151ff61e5d374bea6d04563285acc50dcb11
Ashique-Desai <desai.ashique@gmail.com>
2022-05-18 10:06:39 +0530

    debugging SectionMens.js, text not visible in mobile, added sm:mix-blend-normal for larger screens to remove mix-blend difference property

--- components/SectionMens.js
+++ components/SectionMens.js
@@ -19,7 +19,7 @@ export default function SectionMens() {
                     </div>
                     {/* experiment */}
                     <span className='left-0 top-0 mt-5 relative w-full sm:bottom-6 sm:absolute sm:w-1/2'>
-                        <div className='relative text-white sm:absolute sm:bottom-0 sm:left-0 lg:ml-16 ml-6 mr-6 lg:mb-20 mb-10 z-[40] mix-blend-difference'>
+                        <div className='relative text-white sm:absolute sm:bottom-0 sm:left-0 lg:ml-16 ml-6 mr-6 lg:mb-20 mb-10 z-[40] sm:mix-blend-normal mix-blend-difference'>
                             <CallToAction title='Footwear' description='AMIRI’s singular journey cornerstones the 
                                 Autumn-Winter 2022 collection, which marks Mike Amiri’s debut 
                                 runway show in his home of Los Angeles – a 
